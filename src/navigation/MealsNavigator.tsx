0d30c1d52208cdbed4bc581fea5efe8a5fc23326
Felipe Rocha <felipemota.rocha@gmail.com>
2020-12-01 23:00:08 -0300

    feat: tab navigation

--- src/navigation/MealsNavigator.tsx
+++ src/navigation/MealsNavigator.tsx
@@ -1,10 +1,14 @@
+import * as React from "react";
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import COLORS from "../constants/colors.constant";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
+import FavoritesScreen from "../screens/FavoritesScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 
 const MealsNavigator = createStackNavigator(
@@ -25,4 +29,31 @@ const MealsNavigator = createStackNavigator(
 	}
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator(
+	{
+		Meals: {
+			screen: MealsNavigator,
+			navigationOptions: {
+				tabBarIcon: (tabInfo) => (
+					<Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
+				),
+			},
+		},
+		Favorites: {
+			screen: FavoritesScreen,
+			navigationOptions: {
+				tabBarLabel: "Favorites!",
+				tabBarIcon: (tabInfo) => (
+					<Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
+				),
+			},
+		},
+	},
+	{
+		tabBarOptions: {
+			activeTintColor: COLORS.accentColor,
+		},
+	}
+);
+
+export default createAppContainer(MealsFavTabNavigator);
